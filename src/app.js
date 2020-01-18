db2a28ffe0c04bf8120b214e91cd5e0972cb241b
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-18 09:20:59 -0700

    Organize app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
+const { NODE_ENV } = require("./config");
+
+/* Middleware */
 const cors = require("cors");
 const helmet = require("helmet");
-const { NODE_ENV } = require("./config");
+const morgan = require("morgan");
+
+/* Routers */
 
 const app = express();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
+/* Middleware use methods before Routers */
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+// Return 500 for internal server errros
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
@@ -24,8 +30,11 @@ app.use(function errorHandler(error, req, res, next) {
   res.status(500).json(response);
 });
 
+// Initial test endpoint
 app.get("/", (req, res) => {
   res.send("Hello, boilerplate!");
 });
+
+// Routers
 
 module.exports = app;
