efa73d59a1a39636a1c15445ca0ad13ab306596e
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-19 08:55:37 -0700

    Went through all code again, everything seems to be updated, migration still failing

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: "postgresql://dunder_mifflin@localhost/balance",
+  DB_URL:
+    process.env.DB_URL || "postgresqul://dunder_mifflin@localhost/balance",
   TEST_DB_URL: "postgresql://dunder_mifflin@localhost/balance-test"
 };
