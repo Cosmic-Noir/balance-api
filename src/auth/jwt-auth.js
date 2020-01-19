4b42d3ad018d22188c178eceece6ac0f8ad30e3a
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-19 12:16:15 -0700

    Update jwt-auth.js

---
+++ src/auth/jwt-auth.js
@@ -0,0 +1,39 @@
+const AuthService = require("./auth-service.js");
+
+// Responsible for validating JWT
+function requireAuth(req, res, next) {
+  const authToken = req.get("Authorization") || "";
+
+  let token;
+
+  if (!authToken.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({ error: "Missing Auth Token" });
+  } else {
+    token = authToken.slice(7, authToken.length);
+  }
+
+  try {
+    const payload = AuthService.verifyToken(token);
+
+    AuthService.getUser(req.app.get("db"), payload.sub)
+      .then(user => {
+        if (!user) {
+          return res
+            .status(401)
+            .json({ error: "Unauthorized request - invalid user" });
+        }
+        req.user_id = user.id;
+        next();
+      })
+      .catch(err => {
+        console.log(err);
+        next(err);
+      });
+  } catch (error) {
+    res.status(401).json({ error: "Unauthrozied Request" });
+  }
+}
+
+module.exports = {
+  requireAuth
+};
