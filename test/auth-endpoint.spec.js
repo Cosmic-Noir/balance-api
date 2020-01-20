62814ee96741e4a89459df58d5a9df856decf176
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-20 16:52:33 -0700

    Update postgrator-config to check for Node_env if test

--- test/auth-endpoint.spec.js
+++ test/auth-endpoint.spec.js
@@ -1,1 +1,17 @@
-c;
+const knex = require("knex");
+const app = require("../src/app");
+const jwt = require("jsonwebtoken");
+const { makeUsersArray, makeAuthHeader } = require("./users.fixtures");
+
+let db;
+
+// Create connection to test db:
+before("Make knex instance", () => {
+  db = knex({
+    client: "pg",
+    connection: process.env.TEST_DB_URL
+  });
+  app.set("db", db);
+});
+
+// Disconnect and clear the table for testing
