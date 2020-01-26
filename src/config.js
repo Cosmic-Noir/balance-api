058ac2e497b768c340377c0d9b37d7daf0d980e6
Kristen Doucette <cosmicnoir90@gmail.com>
2020-01-26 15:07:44 -0700

    Update db to database

--- src/config.js
+++ src/config.js
@@ -2,9 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
-    process.env.DB_URL || "postgresqul://dunder_mifflin@localhost/balance",
+    process.env.DATABASE_URL ||
+    "postgresqul://dunder_mifflin@localhost/balance",
   TEST_DATABASE_URL:
-    process.env.TEST_DB_URL ||
+    process.env.TEST_DATABASE_URL ||
     "postgresql://dunder_mifflin@localhost/balance-test",
   JWT_SECRET: process.env.JWT_SECRET
 };
